a1ca4261d757e0e4716e1a1d1fb5a67c9d283b1c
alstn2468 <alstn2468_@naver.com>
2018-09-28 00:41:44 +0900

    2018-09-28

--- Report_1/Step_1/Step_1.c
+++ Report_1/Step_1/Step_1.c
@@ -1,1 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define MATRIX_SIZE 100
+
+int main()
+{
+    int **matrix;
+
+    matrix = (int **)malloc(sizeof(int *) * MATRIX_SIZE);
+
+    for (int i = 0; i < MATRIX_SIZE; i++)
+        matrix[i] = (int *)malloc(sizeof(int) * MATRIX_SIZE);
+
+    return 0;
+}
