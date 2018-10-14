79e66997ef24f7e9ad783a7fe9ea5a421b162cc4
alstn2468 <alstn2468_@naver.com>
2018-10-14 20:30:26 +0900

    2018-10-14

---
+++ Report_1/Step_2/Array_Or_Record/Array_Or_Record.java
@@ -0,0 +1,39 @@
+
+public class Solution {
+
+	class useRecord {
+		int a;
+		char b;
+
+		void setValue(int a, char b) {
+			this.a = a;
+			this.b = b;
+		}
+
+		void display() {
+			System.out.println("useRecord : " + this.a + this.b);
+		}
+	}
+
+	public static void main(String[] args) {
+		int a[] = new int[10];
+
+		for (int i = 0; i < a.length; i++)
+			a[i] = (i + 1);
+
+		System.out.print("Use Array : ");
+
+		for (int i = 0; i < a.length; i++)
+			System.out.print(a[i] + ' ');
+
+		System.out.println();
+
+		useRecord record[] = new useRecord[10];
+
+		for (int i = 0; i < record.length; i++) {
+			record[i].setValue(i + 1, (char)('a' + i));
+			record[i].display();
+		}
+	}
+
+}
