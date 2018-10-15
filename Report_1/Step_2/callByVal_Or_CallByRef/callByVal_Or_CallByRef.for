37ab62b3ddf56dca7a62541372049e344bf4dc2a
alstn2468 <alstn2468_@naver.com>
2018-10-15 20:02:18 +0900

    Add CallByVal_Or_CallByRef.for

---
+++ Report_1/Step_2/callByVal_Or_CallByRef/callByVal_Or_CallByRef.for
@@ -0,0 +1,28 @@
+PROGRAM CallByVal_Or_CallByRef
+    IMPLICIT NONE
+
+    INTEGER :: a, b
+
+    a = 1
+    b = 2
+
+    Print *, 'a : ', a, ', b : ',  b
+    ! a : 1, b : 2
+
+    CALL Swap(a, b)
+
+    Print *, 'a : ', a, ' b : ',  b
+    ! a : 2, b : 1
+
+    CONTAINS
+    SUBROUTINE Swap(A, B)
+        INTEGER, INTENT(IN) :: A, B
+        INTEGER :: TEMP
+
+        TEMP = A
+        A = B
+        B = TEMP
+
+    END SUBROUTINE
+
+END PROGRAM CallByVal_Or_CallByRef
