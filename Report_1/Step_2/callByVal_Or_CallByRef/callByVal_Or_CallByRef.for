fef0577fd0a0cfe4e580b306ce30be60958a9674
alstn2468 <alstn2468_@naver.com>
2018-10-15 20:23:28 +0900

    Test CallByVal_Or_CallByRef.for and Fix Source code and Comment

--- Report_1/Step_2/callByVal_Or_CallByRef/callByVal_Or_CallByRef.for
+++ Report_1/Step_2/callByVal_Or_CallByRef/callByVal_Or_CallByRef.for
@@ -1,28 +1,25 @@
 PROGRAM CallByVal_Or_CallByRef
-    IMPLICIT NONE
-
     INTEGER :: a, b
 
     a = 1
     b = 2
 
     Print *, 'a : ', a, ', b : ',  b
-    ! a : 1, b : 2
+    ! a :            1 , b :            2
 
     CALL Swap(a, b)
 
-    Print *, 'a : ', a, ' b : ',  b
-    ! a : 2, b : 1
+    Print *, 'a : ', a, ', b : ',  b
+    ! a :            2 , b :            1
 
     CONTAINS
     SUBROUTINE Swap(A, B)
-        INTEGER, INTENT(IN) :: A, B
-        INTEGER :: TEMP
+    INTEGER, INTENT(INOUT) :: A, B
+    INTEGER :: TEMP
 
-        TEMP = A
-        A = B
-        B = TEMP
+    TEMP = A
+    A = B
+    B = TEMP
 
     END SUBROUTINE
-
 END PROGRAM CallByVal_Or_CallByRef
